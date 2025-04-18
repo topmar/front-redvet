7bd37776ed3ed6bd7b6419a9b6c8d2ff5756f546
Topolski <topolskimarcin@gmail.com>
2025-04-18 21:57:00 +0200

    nav component created

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,14 @@
+"use client"
 import { useMessages, useTranslations } from 'next-intl'
 import Link from 'next/link'
+import { useSelectedLayoutSegment } from 'next/navigation'
 
 export const Nav = () => {
   const t = useTranslations('Navigation')
   const links = Object.keys(useMessages().Navigation)
+
   return (
-    <nav className="fs-[2rem] fw-bold hidden md:block">
+    <nav className="hidden md:block">
       <ul className="flex">
         {links.map((key) => (
           <li key={key}>
@@ -14,8 +17,7 @@ export const Nav = () => {
               className="
               inline-flex items-center justify-center gap-2 whitespace-nowrap shrink-0 h-10 px-6
               text-[1.125rem] font-medium transition-all
-              outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[2px]
-              hover:bg-black hover:text-white hover:bg-accent hover:text-accent-foreground"
+              outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[2px] hover:bg-black hover:text-white"
             >
               {t(key)}
             </Link>
